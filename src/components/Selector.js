a1c94240eec9fd7385722c980be9145f3e6b8f7c
iVinci-RyosukeMISHIMA <ryosuke.mishima@i-vinci.co.jp>
2024-01-21 15:33:57 +0900

    ~p.123

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -4,18 +4,25 @@ import countriesJson from "../countries.json";
 
 const Selector = () => {
 
-  const[country,setCountry]=useState("");
+  const [country, setCountry] = useState("");
 
-  return(
+  const getCountryData = () => {
+    fetch("https://monotein-books.vercel.app/api/corona-tracker/country/" + country)
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
+
+  return (
     <div>
       <select onChange={e => setCountry(e.target.value)}>
+        <option>Select A Country</option>
         {
-          countriesJson.map((country, index)=>
+          countriesJson.map((country, index) =>
             <option key={index} value={country.Slug}>{country.Country}</option>
-          )        
+          )
         }
       </select>
-      {country}
+      <button onClick={getCountryData}>fetch</button>
     </div>
   )
 }

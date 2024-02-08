f15c82a8401516912808d37e604eb45bcedac2a0
iVinci-RyosukeMISHIMA <ryosuke.mishima@i-vinci.co.jp>
2024-02-08 17:52:41 +0900

    TopPageのみ使用するStateなどをTopPage.tsxに移動

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,7 @@ import './App.css';
 import { useEffect, useState } from "react";
 import TopPage from './pages/TopPage';
 import WorldPage from './pages/WorldPage';
-import countriesJson from "./countries.json";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-
-type CountrydataType = {
-  date: string
-  newConfirmed: number
-  totalConfirmed: number
-  newRecovered: number
-  totalRecovered: number
-}
 
 interface SingleCountryDataType {
   Country: string
@@ -23,44 +14,13 @@ interface AllCountriesDataType extends Array<SingleCountryDataType> { }
 
 function App() {
 
-  const [country, setCountry] = useState<string>("japan");
-  const [countryData, setCountryData] = useState<CountrydataType>({
-    date: "",
-    newConfirmed: 0,
-    totalConfirmed: 0,
-    newRecovered: 0,
-    totalRecovered: 0
-  })
   const [allCountriesData, setAllCountriesData] = useState<AllCountriesDataType>([
     {
       Country: "",
       NewConfirmed: 0,
       TotalConfirmed: 0
     }
-  ]
-  );
-  const [loading, setLoading] = useState<boolean>(false);
-
-
-
-  useEffect(() => {
-    const getCountryData = () => {
-      setLoading(true);
-      fetch("https://monotein-books.vercel.app/api/corona-tracker/country/" + country)
-        .then(res => res.json())
-        .then(data => setCountryData({
-          date: new Date(data[data.length - 1].Date).getFullYear() + "-" + (new Date(data[data.length - 1].Date).getMonth() + 1) + "-" + new Date(data[data.length - 1].Date).getDate(),
-          newConfirmed: data[data.length - 1].Confirmed - data[data.length - 2].Confirmed,
-          totalConfirmed: data[data.length - 1].Confirmed,
-          newRecovered: data[data.length - 1].Recovered - data[data.length - 2].Recovered,
-          totalRecovered: data[data.length - 1].Recovered
-        })
-        )
-        .then(a => setLoading(false))
-        .catch(err => alert("Error occured. Please reload the page and try again."))
-    }
-    getCountryData();
-  }, [country])
+  ]);
 
   useEffect(() => {
     fetch("https://monotein-books.vercel.app/api/corona-tracker/summary")
@@ -72,7 +32,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TopPage countriesJson={countriesJson} setCountry={setCountry} countryData={countryData} loading={loading}></TopPage>} />
+        <Route path="/" element={<TopPage ></TopPage>} />
         <Route path="world" element={<WorldPage allCountriesData={allCountriesData}></WorldPage>} />
       </Routes>
     </BrowserRouter>
--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -2,24 +2,48 @@ import Header from "../components/Header";
 import Title from "../components/Title";
 import Selector from "../components/Selector";
 import Results from "../components/Results";
+import { useEffect, useState } from "react";
+import countriesJson from "../countries.json";
 
-type TopPageType = {
-  countriesJson: {
-    Country: string
-    Slug: string
-  }[]
-  setCountry: React.Dispatch<React.SetStateAction<string>>
-  countryData: {
-    date: string
-    newConfirmed: number
-    totalConfirmed: number
-    newRecovered: number
-    totalRecovered: number
-  },
-  loading: boolean
+type CountrydataType = {
+  date: string
+  newConfirmed: number
+  totalConfirmed: number
+  newRecovered: number
+  totalRecovered: number
 }
 
-const TopPage = ({ countriesJson, setCountry, countryData, loading }: TopPageType) => {
+const TopPage = () => {
+  const [country, setCountry] = useState<string>("japan");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [countryData, setCountryData] = useState<CountrydataType>({
+    date: "",
+    newConfirmed: 0,
+    totalConfirmed: 0,
+    newRecovered: 0,
+    totalRecovered: 0
+  })
+
+  useEffect(() => {
+    const getCountryData = () => {
+      setLoading(true);
+      fetch("https://monotein-books.vercel.app/api/corona-tracker/country/" + country)
+        .then(res => res.json())
+        .then(data => setCountryData({
+          date: new Date(data[data.length - 1].Date).getFullYear() + "-" + (new Date(data[data.length - 1].Date).getMonth() + 1) + "-" + new Date(data[data.length - 1].Date).getDate(),
+          newConfirmed: data[data.length - 1].Confirmed - data[data.length - 2].Confirmed,
+          totalConfirmed: data[data.length - 1].Confirmed,
+          newRecovered: data[data.length - 1].Recovered - data[data.length - 2].Recovered,
+          totalRecovered: data[data.length - 1].Recovered
+        })
+        )
+        .then(a => setLoading(false))
+        .catch(err => alert("Error occured. Please reload the page and try again."))
+    }
+    getCountryData();
+  }, [country])
+
   return (
     <div>
       <Header />

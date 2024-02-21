daff6807db664bb0dbdf053b0397c23dc5d412a8
iVinci-RyosukeMISHIMA <ryosuke.mishima@i-vinci.co.jp>
2024-02-21 11:48:54 +0900

    setCountryDataの処理を切り出し

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -25,19 +25,24 @@ const TopPage = () => {
     totalRecovered: 0
   })
 
+  const createObjectForState = function (data: any) {
+    const todaysData = data[data.length - 1];
+    const yesterdaysData = data[data.length - 2];
+    setCountryData({
+      date: new Date(todaysData.Date).getFullYear() + "-" + (new Date(todaysData.Date).getMonth() + 1) + "-" + new Date(todaysData.Date).getDate(),
+      newConfirmed: todaysData.Confirmed - yesterdaysData.Confirmed,
+      totalConfirmed: todaysData.Confirmed,
+      newRecovered: todaysData.Recovered - yesterdaysData.Recovered,
+      totalRecovered: todaysData.Recovered
+    })
+  }
+
   useEffect(() => {
     const getCountryData = () => {
       setLoading(true);
       fetch("https://monotein-books.vercel.app/api/corona-tracker/country/" + country)
         .then(res => res.json())
-        .then(data => setCountryData({
-          date: new Date(data[data.length - 1].Date).getFullYear() + "-" + (new Date(data[data.length - 1].Date).getMonth() + 1) + "-" + new Date(data[data.length - 1].Date).getDate(),
-          newConfirmed: data[data.length - 1].Confirmed - data[data.length - 2].Confirmed,
-          totalConfirmed: data[data.length - 1].Confirmed,
-          newRecovered: data[data.length - 1].Recovered - data[data.length - 2].Recovered,
-          totalRecovered: data[data.length - 1].Recovered
-        })
-        )
+        .then(data => createObjectForState(data))
         .then(a => setLoading(false))
         .catch(err => alert("Error occured. Please reload the page and try again."))
     }
